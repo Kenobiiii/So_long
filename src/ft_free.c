c7e03dc8af5fb2b1f42ee87d5badd6821a192e4c
Pablo Pons <pablo.romeroponsfuster@gmail.com>
2024-05-20 19:59:02 +0200

    Arreglado free infinito

--- src/ft_free.c
+++ src/ft_free.c
@@ -6,7 +6,7 @@
 /*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 09:22:44 by paromero          #+#    #+#             */
-/*   Updated: 2024/05/13 12:05:49 by paromero         ###   ########.fr       */
+/*   Updated: 2024/05/20 17:48:17 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void	free_matriz(char **matriz, t_res *game)
 	while (i < game->height)
 	{
 		free(matriz[i]);
+		i++;
 	}
 	free(matriz);
 }
@@ -29,9 +30,13 @@ void	free_all(t_res	*game)
 	if (game)
 	{
 		if (game->map)
+		{
 			free_matriz(game->map, game);
+			game->map = NULL;
+		}
 		// if (game)
 		// 	free_map_txt(game);
 		free(game);
+		game = NULL;
 	}
 }
